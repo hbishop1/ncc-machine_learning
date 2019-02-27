4c5744f279686d8e4f73ca7bd66a9be3e2ff370a
hbishop1 <hughbishop@live.co.uk>
2019-02-27 11:37:25 +0000

    added random resized crop

--- coursework.py
+++ coursework.py
@@ -19,6 +19,7 @@ class_names = ['apple','aquarium_fish','baby','bear','beaver','bed','bee','beetl
 
 train_loader = torch.utils.data.DataLoader(
     torchvision.datasets.CIFAR100('data', train=True, download=True, transform=torchvision.transforms.Compose([
+        torchvision.transforms.RandomResizedCrop(32),
         torchvision.transforms.RandomRotation(30),
         torchvision.transforms.RandomHorizontalFlip(),
         torchvision.transforms.ToTensor()
