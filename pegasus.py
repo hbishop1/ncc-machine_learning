bd441adf8bcf24a6ccdb8720315ea880ca40facd
hbishop1 <hughbishop@live.co.uk>
2019-02-28 12:40:20 +0000

    added another linear layer and increased neurons

--- pegasus.py
+++ pegasus.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
 import matplotlib.pyplot as plt
+from torchvision.utils import save_image
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
@@ -22,82 +23,121 @@ train_loader = torch.utils.data.DataLoader(
     ])),
 shuffle=True, batch_size=16, drop_last=True)
 
-test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.CIFAR10('data', train=False, download=True, transform=torchvision.transforms.Compose([
-        torchvision.transforms.ToTensor()
-    ])),
-shuffle=False, batch_size=16, drop_last=True)
-
 train_iterator = iter(cycle(train_loader))
-test_iterator = iter(cycle(test_loader))
 
 print(f'> Size of training dataset {len(train_loader.dataset)}')
-print(f'> Size of test dataset {len(test_loader.dataset)}')
 
-plt.figure(figsize=(10,10))
-for i in range(25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(test_loader.dataset[i][0].permute(0,2,1).contiguous().permute(2,1,0), cmap=plt.cm.binary)
-    plt.xlabel(class_names[test_loader.dataset[i][1]])
 
-# define the model (a simple autoencoder)
-class MyNetwork(nn.Module):
-    def __init__(self):
-        super(MyNetwork, self).__init__()
-        layers = nn.ModuleList()
-        layers.append(nn.Linear(in_features=3*32*32, out_features=512))
-        layers.append(nn.ReLU())
-        layers.append(nn.Linear(in_features=512, out_features=32))
-        layers.append(nn.ReLU())
-        layers.append(nn.Linear(in_features=32, out_features=512))
-        layers.append(nn.ReLU())
-        layers.append(nn.Linear(in_features=512, out_features=3*32*32))
-        layers.append(nn.Sigmoid())
-        self.layers = layers
+class VAE(nn.Module):
+    def __init__(self, intermediate_size=128, hidden_size=20):
+        super(VAE, self).__init__()
+
+        # encoder
+        self.conv1 = nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(3, 32, kernel_size=2, stride=2, padding=0)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(16 * 16 * 32, intermediate_size)
+
+        # latent space
+        self.fc21 = nn.Linear(intermediate_size, hidden_size)
+        self.fc22 = nn.Linear(intermediate_size, hidden_size)
+
+        # decoder
+        self.fc3 = nn.Linear(hidden_size, intermediate_size)
+        self.fc4 = nn.Linear(intermediate_size, 8192)
+        self.deconv1 = nn.ConvTranspose2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.deconv2 = nn.ConvTranspose2d(32, 32, kernel_size=3, stride=1, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(32, 32, kernel_size=2, stride=2, padding=0)
+        self.conv5 = nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1)
+
+    def encode(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.relu(self.conv2(out))
+        out = F.relu(self.conv3(out))
+        out = F.relu(self.conv4(out))
+        out = out.view(out.size(0), -1)
+        h1  = F.relu(self.fc1(out))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = std.data.new(std.size()).normal_()
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def decode(self, z):
+        h3  = F.relu(self.fc3(z))
+        out = F.relu(self.fc4(h3))
+        out = out.view(out.size(0), 32, 16, 16)
+        out = F.relu(self.deconv1(out))
+        out = F.relu(self.deconv2(out))
+        out = F.relu(self.deconv3(out))
+        out = torch.sigmoid(self.conv5(out))
+        return out
 
     def forward(self, x):
-        z = self.encode(x)
-        x = self.decode(z)
-        return x
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
 
-    # encode (flatten as linear, then run first half of network)
-    def encode(self, x):
-        x = x.view(x.size(0), -1)
-        for i in range(4):
-            x = self.layers[i](x)
-        return x
 
-    # decode (run second half of network then unflatten)
-    def decode(self, x):
-        for i in range(4,8):
-            x = self.layers[i](x)
-        x = x.view(x.size(0), 3, 32, 32)
-        return x
-
-N = MyNetwork().to(device)
+N = VAE().to(device)
 
 print(f'> Number of network parameters {len(torch.nn.utils.parameters_to_vector(N.parameters()))}')
 
 # initialise the optimiser
 optimiser = torch.optim.Adam(N.parameters(), lr=0.001)
-epoch = 0
+num_epochs = 5
 
-example_1 = torchvision.transforms.ToTensor()(test_loader.dataset.test_data[13]).to(device)  # horse
-example_2 = torchvision.transforms.ToTensor()(test_loader.dataset.test_data[160]).to(device) # bird
+# VAE loss has a reconstruction term and a KL divergence term summed over all elements and the batch
+def vae_loss(p, x, mu, logvar):
+    BCE = F.binary_cross_entropy(p.view(-1, 32 * 32 * 3), x.view(-1, 32 * 32 * 3), reduction='sum')
 
-example_1_code = N.encode(example_1.unsqueeze(0))
-example_2_code = N.encode(example_2.unsqueeze(0))
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
-# this is some sad blurry excuse of a Pegasus, hopefully you can make a better one
-bad_pegasus = N.decode(0.9*example_1_code + 0.1*example_2_code).squeeze(0)
+    return BCE + KLD
 
-plt.clf()
-plt.grid(False)
-plt.imshow(bad_pegasus.cpu().data.permute(0,2,1).contiguous().permute(2,1,0), cmap=plt.cm.binary)
-plt.show()
+# training loop, feel free to also train on the test dataset if you like
+for epoch in range(1,num_epochs+1):
+
+    print('-' * 10)
+    print('Epoch {}/{}'.format(epoch,num_epochs))
+    
+    # arrays for metrics
+    logs = {}
+    train_loss_arr = np.zeros(0)
+
+    # iterate over some of the train dateset
+    for i in range(1000):
+        x,t = next(train_iterator)
+        x,t = x.to(device), t.to(device)
+
+        optimiser.zero_grad()
+        
+        p, mu, logvar = N(x)
+        loss = vae_loss(p, x, mu, logvar)
+        loss.backward()
+        optimiser.step()
+
+        train_loss_arr = np.append(train_loss_arr, loss.cpu().data)
+
+    print('Train Loss: {:.4f}'.format(train_loss_arr.mean()))
+
+    epoch = epoch+1
+
+for p in range(10):
+    with torch.no_grad():
+        sample = torch.randn(64, 20).to(device)
+        sample = N.decode(sample).cpu()
+        save_image(sample.view(64, 3, 32, 32),
+            'pegasus_{}.png'.format(p))
 
 #for i in range(len(test_loader.dataset.test_labels)):
 #  print(class_names[test_loader.dataset.test_labels[i]] + '\t idx: ' + str(i))
