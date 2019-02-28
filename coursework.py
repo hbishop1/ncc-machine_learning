2fb4a35d2b738109b8c7de6581c31e2b0ec83db0
hbishop1 <hughbishop@live.co.uk>
2019-02-28 10:45:35 +0000

    added two more dench conv layers

--- coursework.py
+++ coursework.py
@@ -74,7 +74,15 @@ class MyNetwork(nn.Module):
         layers.append(nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1))
         layers.append(nn.ReLU())
         layers.append(nn.BatchNorm2d(384))
+
+        layers.append(nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1))
+        layers.append(nn.ReLU())
+        layers.append(nn.BatchNorm2d(384))
         layers.append(nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+
+        layers.append(nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1))
+        layers.append(nn.ReLU())
+        layers.append(nn.BatchNorm2d(384))
 
         layers.append(nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1))
         layers.append(nn.ReLU())
@@ -114,6 +122,9 @@ num_epochs = 3000
 logs = {'train_acc':[],'train_loss':[],'test_acc':[],'test_loss':[]}
 #liveplot = PlotLosses()
 
+open('results.txt','w').close()
+
+
 for epoch in range(1,num_epochs+1):
 
     print('-' * 10)
@@ -151,7 +162,7 @@ for epoch in range(1,num_epochs+1):
         test_loss_arr = np.append(test_loss_arr, loss.cpu().data)
         test_acc_arr = np.append(test_acc_arr, pred.data.eq(t.view_as(pred)).float().mean().item())
 
-    with open('results1.txt','w') as results:
+    with open('results.txt','a') as results:
         results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
         results.write('Train Loss: {:.4f} Train Acc: {:.4f} \n'.format(train_loss_arr.mean(),train_acc_arr.mean()))
         results.write('Test Loss: {:.4f} Test Acc: {:.4f} \n'.format(test_loss_arr.mean(),test_acc_arr.mean()))
