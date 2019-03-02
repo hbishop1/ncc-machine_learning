ee0501f4f99a9e72ad3673df351631dad1b0c5e0
hbishop1 <hughbishop@live.co.uk>
2019-03-02 11:31:56 +0000

    alternating kernel size

--- coursework.py
+++ coursework.py
@@ -122,7 +122,7 @@ num_epochs = 1000
 logs = {'train_acc':[],'train_loss':[],'test_acc':[],'test_loss':[]}
 #liveplot = PlotLosses()
 
-open('results1.txt','w').close()
+open('results.txt','w').close()
 
 
 for epoch in range(1,num_epochs+1):
@@ -162,7 +162,7 @@ for epoch in range(1,num_epochs+1):
         test_loss_arr = np.append(test_loss_arr, loss.cpu().data)
         test_acc_arr = np.append(test_acc_arr, pred.data.eq(t.view_as(pred)).float().mean().item())
 
-    with open('results1.txt','a') as results:
+    with open('results.txt','a') as results:
         results.write('Epoch {}/{} \n'.format(epoch,num_epochs))
         results.write('Train Loss: {:.4f} Train Acc: {:.4f} \n'.format(train_loss_arr.mean(),train_acc_arr.mean()))
         results.write('Test Loss: {:.4f} Test Acc: {:.4f} \n'.format(test_loss_arr.mean(),test_acc_arr.mean()))
