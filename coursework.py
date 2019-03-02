05ca91dd0b77d432f07ba7282003a764ef8b46e2
hbishop1 <hughbishop@live.co.uk>
2019-03-02 10:57:49 +0000

    removed some augmentation shit

--- coursework.py
+++ coursework.py
@@ -19,8 +19,6 @@ class_names = ['apple','aquarium_fish','baby','bear','beaver','bed','bee','beetl
 
 train_loader = torch.utils.data.DataLoader(
     torchvision.datasets.CIFAR100('data', train=True, download=True, transform=torchvision.transforms.Compose([
-        torchvision.transforms.RandomAffine(15),
-        torchvision.transforms.RandomRotation(30),
         torchvision.transforms.RandomHorizontalFlip(),
         torchvision.transforms.ColorJitter(hue=.05, saturation=.05),
         torchvision.transforms.RandomResizedCrop(size=32,scale=(0.5,1)),
