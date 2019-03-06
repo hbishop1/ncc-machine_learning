b6050c019360ef29ae4f8366d65da090b42d892f
hbishop1 <hughbishop@live.co.uk>
2019-03-06 09:26:42 +0000

    high learning rate

--- coursework.py
+++ coursework.py
@@ -117,7 +117,7 @@ N = MyNetwork().to(device)
 print('> Number of network parameters: ', len(torch.nn.utils.parameters_to_vector(N.parameters())))
 
 # initialise the optimiser
-optimiser = torch.optim.Adam(N.parameters(), lr=0.00001, weight_decay=0.01)
+optimiser = torch.optim.Adam(N.parameters(), lr=0.00005, weight_decay=0.005)
 num_epochs = 1000
 logs = {'train_acc':[],'train_loss':[],'test_acc':[],'test_loss':[]}
 #liveplot = PlotLosses()
