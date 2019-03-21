4858dcff04e25ee11b2fd754b88952b047d685db
hbishop1 <hughbishop@live.co.uk>
2019-03-21 15:11:33 +0000

    peg

--- pegasus.py
+++ pegasus.py
@@ -38,18 +38,20 @@ class VAE(nn.Module):
     def __init__(self, intermediate_size=256, hidden_size=40):
         super(VAE, self).__init__()
 
+        self.pool = nn.MaxPool2d(2, 2)
+        self.up = nn.UpsamplingNearest2d(scale_factor=2)
 
         # encoder
         self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1)
         self.batch1 = nn.BatchNorm2d(32)
 
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=2, stride=2, padding=0)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.batch2 = nn.BatchNorm2d(64)
 
-        self.conv3 = nn.Conv2d(64, 128, kernel_size=2, stride=2, padding=0)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
         self.batch3 = nn.BatchNorm2d(128)
 
-        self.conv4 = nn.Conv2d(128, 128, kernel_size=2, stride=2, padding=0)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
         self.batch4 = nn.BatchNorm2d(128)
 
         self.fc1 = nn.Linear(4 * 4 * 128, intermediate_size)
@@ -63,28 +65,25 @@ class VAE(nn.Module):
         self.fc3 = nn.Linear(hidden_size, intermediate_size)
         self.batch6 = nn.BatchNorm1d(intermediate_size)
 
-        self.fc4 = nn.Linear(intermediate_size, 4 * 4 * 64)
-        self.batch7 = nn.BatchNorm1d(4*4*64)
+        self.fc4 = nn.Linear(intermediate_size, 4 * 4 * 128)
+        self.batch7 = nn.BatchNorm1d(4*4*128)
 
-        self.up1 = nn.UpsamplingNearest2d(scale_factor=2)
-        self.deconv1 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
-        self.batch8 = nn.BatchNorm2d(64)
+        self.deconv1 = nn.ConvTranspose2d(128, 128, kernel_size=3, stride=1, padding=1)
+        self.batch8 = nn.BatchNorm2d(128)
 
-        self.up2 = nn.UpsamplingNearest2d(scale_factor=2)
-        self.deconv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
-        self.batch9 = nn.BatchNorm2d(128)
+        self.deconv2 = nn.ConvTranspose2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.batch9 = nn.BatchNorm2d(64)
 
-        self.up3 = nn.UpsamplingNearest2d(scale_factor=2)
-        self.deconv3 = nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1)
-        self.batch10 = nn.BatchNorm2d(128)
-
-        self.conv5 = nn.Conv2d(128, 3, kernel_size=3, stride=1, padding=1)
+        self.deconv3 = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=1, padding=1)
+        self.batch10 = nn.BatchNorm2d(32)
+        
+        self.deconv4 = nn.ConvTranspose2d(32, 3, kernel_size=3, stride=1, padding=1)
 
     def encode(self, x):
         out = self.batch1(F.relu(self.conv1(x)))
-        out = self.batch2(F.relu(self.conv2(out)))
-        out = self.batch3(F.relu(self.conv3(out)))
-        out = self.batch4(F.relu(self.conv4(out)))
+        out = self.batch2(self.pool(F.relu(self.conv2(out))))
+        out = self.batch3(self.pool(F.relu(self.conv3(out))))
+        out = self.batch4(self.pool(F.relu(self.conv4(out))))
         out = out.view(out.size(0), -1)
         h1  = self.batch5(F.relu(self.fc1(out)))
         return self.fc21(h1), self.fc22(h1)
@@ -100,11 +99,11 @@ class VAE(nn.Module):
     def decode(self, z):
         h3  = self.batch6(F.relu(self.fc3(z)))
         out = self.batch7(F.relu(self.fc4(h3)))
-        out = out.view(out.size(0), 64, 4, 4)
-        out = self.batch8(F.relu(self.deconv1(self.up1(out))))
-        out = self.batch9(F.relu(self.deconv2(self.up2(out))))
-        out = self.batch10(F.relu(self.deconv3(self.up3(out))))
-        out = torch.sigmoid(self.conv5(out))
+        out = out.view(out.size(0), 128, 4, 4)
+        out = self.batch8(F.relu(self.deconv1(self.up(out))))
+        out = self.batch9(F.relu(self.deconv2(self.up(out))))
+        out = self.batch10(F.relu(self.deconv3(self.up(out))))
+        out = torch.sigmoid(self.deconv4(out))
         return out
 
     def forward(self, x):
@@ -165,7 +164,7 @@ for epoch in range(1,num_epochs+1):
     epoch = epoch+1
 
 with torch.no_grad():
-    sample = torch.randn(16, 40).to(device)
+    sample = torch.randn(64, 40).to(device)
     sample = N.decode(sample).cpu()
-    save_image(sample.view(16, 3, 32, 32),'pegasus2.png')
+    save_image(sample.view(64, 3, 32, 32),'pegasus2.png')
 
